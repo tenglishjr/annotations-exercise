b20bffda499a6e53ff7fe8671b4d86016c208170
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-06 10:22:58 -0500

    Solved with annotations

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-@SuppressWarnings("all")
 public class Main {
-    public static void main(String[] args) {
+     @SuppressWarnings("deprecation")
+     public static void main(String[] args) {
         Author author = new Author("Sandi", "Metz");
 
         author.addBook("Practical Object-Oriented Design in Ruby");
